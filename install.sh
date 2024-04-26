62c1a56e8ae08abdbd7bdc3be0ec64d87ba6960a
SmallMushroom-offical <jxk370@126.com>
2024-04-27 04:16:00 +0800

    使用来自openSUSE的gsettings-qt以及修复了一系列的bug

--- install.sh
+++ install.sh
@@ -3,15 +3,17 @@ pkgname=fcitx-sogoupinyin
 pkgver=4.2.1.145
 filename=${pkgname:6}_${pkgver}_amd64.deb
 echo "1.Fedora"
-echo "2.Red Hat Enterprise Linux 7/CentOS 7"
+echo "2.Red Hat Enterprise Linux 7/CentOS 7（未经测试）"
 echo "3.Red Hat Enterprise Linux 8/CentOS 8/Rocky Linux 8/Alma Linux 8"
 read -p "请选择您的系统：" redhatsys
 echo "正在尝试安装依赖"
 if [[ "$redhatsys" == "3" ]]; then
     pacman=yum
     sudo $pacman -y install wget epel-release
+    sudo $pacman makecache
     sudo $pacman -y install fcitx fcitx-qt5 kcm-fcitx
-    wget https://pkgs.sysadmins.ws/el8/base/x86_64/gsettings-qt-0-0.25.20180723bzr85.el8.x86_64.rpm
+    wget https://ftp.lysator.liu.se/pub/opensuse/distribution/leap/15.5/repo/oss/x86_64/gsettings-qt-0.2-bp155.1.15.x86_64.rpm
+    wget https://ftp.lysator.liu.se/pub/opensuse/distribution/leap/15.5/repo/oss/x86_64/libgsettings-qt1-0.2-bp155.1.15.x86_64.rpm
     wget https://mirrors.nju.edu.cn/epel/7/x86_64/Packages/f/fcitx-configtool-0.4.10-1.el7.x86_64.rpm
     wget https://mirrors.nju.edu.cn/epel/7/x86_64/Packages/u/unique-1.1.6-10.el7.x86_64.rpm
     sudo $pacman -y install ./*.rpm
@@ -48,9 +50,9 @@ sudo cp -r $pkgdir/usr/* /usr
 sudo cp -r $pkgdir/etc/* /etc
 sudo cp -r $pkgdir/opt/* /opt
 echo "正在配置fcitx"
-sudo echo 'export GTK_IM_MODULE=fcitx' >> ~/.bashrc
-sudo echo 'export QT_IM_MODULE=fcitx' >> ~/.bashrc
-sudo echo 'export XMODIFIERS=@im=fcitx' >> ~/.bashrc
+sudo echo 'export GTK_IM_MODULE=fcitx' >> /etc/profile
+sudo echo 'export QT_IM_MODULE=fcitx' >> /etc/profile
+sudo echo 'export XMODIFIERS=@im=fcitx' >> /etc/profile
 echo "正在链接共享库..."
 sudo cp /usr/lib/x86_64-linux-gnu/fcitx/fcitx-sogoupinyin.so /usr/lib64/fcitx
 sudo ln -s /usr/lib/x86_64-linux-gnu/fcitx/fcitx-punc-ng.so /usr/lib64/fcitx-punc-ng.so
@@ -60,6 +62,7 @@ echo "正在清除安装后无用的文件"
 rm sogou*.deb
 rm -rf pkg
 rm -rf src
+rm ./*.tar.xz
 if [[ "$redhatsys" == "3" ]]; then
    rm *.rpm
 fi
